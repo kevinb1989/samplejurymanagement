ad5420ff32bcb9eddb17dcd21ba132a8351fc125
kevinb1989 <hoanglongbui89@yahoo.com.vn>
2015-01-06 17:56:18 +1100

    implement Paypal One Day Test Plan
    implement Paypal One Day Test Plan

--- app/views/subscribe.blade.php
+++ app/views/subscribe.blade.php
@@ -6,10 +6,19 @@
 
 @section('content')
 	<section class="panel">
-		<header class="panel-heading">Subscribe with Paypal</header>
+		<header class="panel-heading">Subscribe to TAN Sponsored Pro</header>
 		<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="CW92LVG24R6K6">
+			<input type="image" src="https://www.sandbox.paypal.com/en_AU/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+			<img alt="" border="0" src="https://www.sandbox.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+		</form>
+	</section>
+	<section class="panel">
+		<header class="panel-heading">Subscribe to Paypal TAN One Day Test Plan</header>
+		<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="QZRWPZTWQU34N">
 			<input type="image" src="https://www.sandbox.paypal.com/en_AU/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
 			<img alt="" border="0" src="https://www.sandbox.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
 		</form>

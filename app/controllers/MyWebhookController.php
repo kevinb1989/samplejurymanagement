9996ed0a5f2b26ad463110578cbf71b3b1d04ea4
kevinb1989 <hoanglongbui89@yahoo.com.vn>
2015-01-08 12:53:04 +1100

    modify MyWebhookController.php again

--- app/controllers/MyWebhookController.php
+++ app/controllers/MyWebhookController.php
@@ -1,5 +1,17 @@
 <?php
 
 class MyWebhookController extends Laravel\Cashier\WebhookController {
+	/**
+	 * Determine if the invoice has too many failed attempts.
+	 * Maximum number of failed attempts have been reduced to 1 for the purpose of testing
+	 *
+	 * @param  array  $payload
+	 * @return bool
+	 */
+	protected function tooManyFailedPayments(array $payload)
+	{
+		return $payload['data']['object']['attempt_count'] > 0;
+	}
+
 
 }

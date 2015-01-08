b36c99608d6c8e26c869af21b2f6d28a21aed07d
kevinb1989 <hoanglongbui89@yahoo.com.vn>
2015-01-08 11:58:16 +1100

    modify MyWebhookController.php

--- app/controllers/MyWebhookController.php
+++ app/controllers/MyWebhookController.php
@@ -2,6 +2,15 @@
 
 class MyWebhookController extends Laravel\Cashier\WebhookController {
 
-	
+	/**
+	 * Overide missingMethod from WebhookController to test Stripe Webhook
+	 *
+	 * @param  array   $parameters
+	 * @return mixed
+	 */
+	public function missingMethod($parameters = array())
+	{
+		return new Response('Handling method is missing', 200);;
+	}
 
 }
